a6a30a140406a44ebea23e7cd2c990fd43c30390
章颢 <zhanghao@buding.cn>
2013-09-04 17:08:10 +0800

    fix bugs of luajit-iOS6.1.sh

--- luajit-iOS6.1.sh
+++ luajit-iOS6.1.sh
@@ -17,15 +17,17 @@ make HOST_CC="$CC -m32 -arch i386" CROSS=$ISDKP TARGET_FLAGS="-arch armv7s -isys
 cp -p src/libluajit.a iOS/libluajit-armv7s.a
 
 #make for Simulator
-make CC="$CC -m32 -arch i386" clean all
+make CC="$CC -m32" clean all
 cp -p src/libluajit.a iOS/libluajit-i386.a
 
 #combine files
 make clean
-lipo -create iOS/libluajit-*.a -output ../LuaJIT.framework/LuaJIT
+mkdir LuaJIT.framework
+mkdir LuaJIT.framework/Headers
+lipo -create iOS/libluajit-*.a -output ./LuaJIT.framework/LuaJIT
 rm iOS/libluajit-*.a
 
 #copy headers
-cp src/luajit.h src/luaconf.h src/lua.h src/lua.hpp src/lauxlib.h src/lualib.h ../LuaJIT.framework/Headers
+cp src/luajit.h src/luaconf.h src/lua.h src/lua.hpp src/lauxlib.h src/lualib.h ./LuaJIT.framework/Headers
 
 cd ..
